ed11f27ce65e71fec63a7a30fe527e6c8db74fb9
luqianpx <qianqianpx@hotmail.com>
2025-03-12 20:25:59 -0400

    Update AA99_02_test.py

--- AA99_02_test.py
+++ AA99_02_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import support_based as spb
 
-path = 'E:/Project20_ECG_foundation_model/Model/model11_SimCLR_moredata/save/cnntransf_nomix_2_20_2500_20_DzwU/'
+path = 'E:/Project20_ECG_foundation_model/Model/model11_SimCLR/save/cnntransf_nomix_2_20_2500_20_DzwU/'
 pretrain_res = spb.read_res(path, 'prettain-res')
 fineturn_nopretrain_res = spb.read_res(path, 'fineturn-nopretrain-res')
 fineturn_pretrain_full_res = spb.read_res(path, 'fineturn-pretrain_full-res')
